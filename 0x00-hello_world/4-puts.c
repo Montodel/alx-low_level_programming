85342839ba935a68915a833224702b0e89243b16
Montodel <elnenysuleiman@gmail.com>
2022-09-08 10:12:00 -0700

    c is amazing

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+
+ *main - task 4
+
+ *
+
+ *Return: 0 after printing text.
+
+**/
+
+
+
+int main(void)
+
+{
+
+puts("\"Programming is like building a multilingual puzzle\n");
+
+return (0);
+
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+
+ *main - task 5
+
+ *
+
+ *Return: 1
+
+**/
+
+
+
+int main(void)
+
+{
+
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
+
+}
