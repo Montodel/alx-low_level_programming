a1fd7a03b457cae9dbdd95031949a83ba650753a
Montodel <elnenysuleiman@gmail.com>
2022-09-08 10:25:43 -0700

    initial commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,12 @@
 #include <stdio.h>
-
 /**
-
  *main - task 4
-
  *
-
  *Return: 0 after printing text.
-
 **/
 
-
-
 int main(void)
-
 {
-
 puts("\"Programming is like building a multilingual puzzle\n");
-
 return (0);
-
 }
