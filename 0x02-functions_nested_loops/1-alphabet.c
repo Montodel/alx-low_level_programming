6df9f7d5f27934165a73ec982925a9a4a8bffa9f
Montodel <elnenysuleiman@gmail.com>
2022-09-13 08:20:33 -0700

    task 1 completed

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_alphabet - prints the alphabet, in lowercase, using _putchar- task 1
+* Return: 0, void returns nothing.
+*/
+
+void print_alphabet(void)
+{
+char c ;
+for ( c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
