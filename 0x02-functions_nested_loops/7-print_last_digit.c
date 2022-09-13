9b913c61e376330d6a075e0669724d1cd4e7e169
Montodel <elnenysuleiman@gmail.com>
2022-09-13 10:46:45 -0700

    task 5 - 8-24

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit of a number
+ * @n: int type number
+ * Return: return value of last digit
+ */
+
+int print_last_digit(int n)
+{
+	int xy;
+	if (n < 0)
+	{
+		xy = -1 * (n % 10);
+		_putchar(xy + '0');
+		return (xy);
+	}
+	else
+	{
+		xy = n % 10;
+		_putchar(xy + '0');
+		return (xy);
+	}
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Print every minute of the day of Jack Bauer
+ * Description: Start from 00:00 to 23:59 
+*/
+
+void jack_bauer(void)
+{
+int i,j;
+	for (i=0 ; i<24 ; i++)
+	{
+		for (j=0 ; j<=59 ; j++)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+            		_putchar('\n');
+        	}
+    	}
+}
