a586b7ff5a10b54d179f34ed301fe9ca0df1e1bc
Montodel <elnenysuleiman@gmail.com>
2022-09-13 09:53:40 -0700

    task 2-4

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,7 @@
 void print_alphabet_x10(void)
 {
 char c;
-int i=0;
+int i;
 for (i = 0; i <= 10; i++)
 {
 for (c = 'a'; c <= 'z'; c++)
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
 /**
-** _islower - Short description, single line
-* @c: contains value to be compared
-* Return: 1 if lowercase, 0 if not lowercase
-*/
+ *_islower - Short description, single line
+ *@c: contains value to be compared
+ *Return: 1 if lowercase, 0 if not lowercase
+ */
 
-int _islower(int c);
+int _islower(int c)
 {
-if (c > 96 && c < 123)
-{
-	return (1);
-}else {
-	return (0);
-}
+	if (c > 'a' && c < 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
