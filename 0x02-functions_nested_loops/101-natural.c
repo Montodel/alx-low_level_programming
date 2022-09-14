534cedcade77da219b7460cf52ebbd07f06908d9
Montodel <elnenysuleiman@gmail.com>
2022-09-13 23:02:55 -0700

    what a reaction

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,7 +7,7 @@
  * Return: 0
  */
 
- int main(void)
+int main(void)
 {
 	int i = 1;
 	int total = 0;
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 
 /**
 *jack_bauer - Print every minute of the day of Jack Bauer
-*Description: Start from 00:00 to 23:59 
+*Description: Start from 00:00 to 23:59
 */
 
 void jack_bauer(void)
