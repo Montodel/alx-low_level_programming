ec9bd1faad42727988cd055c2e2ebd7619c6b809
Montodel <elnenysuleiman@gmail.com>
2022-09-19 11:13:30 -0700

    updated task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
-/*
- * reset_to_98 - Updates the value it points to 98
- * @n: pointer to n
- * Return: void
+/**
+ * reset_to_98 -> a function to update the preference to a pointer
+ * @n: int data type
+ * Return: Always 0
  */
 
 void reset_to_98(int *n)
