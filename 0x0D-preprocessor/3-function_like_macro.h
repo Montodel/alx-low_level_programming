41833928c8ba1cdd260010130d7ae0a4fe5953ae
Montodel <elnenysuleiman@gmail.com>
2022-10-11 07:57:27 -0700

    First commit

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef _3_FUNTION_LIKE_MACRO_H_
+#define _3_FUNTION_LIKE_MACRO_H_
+
+#define ABS(x) ((x) < 0 ? (-(x)) : (x))
+
+#endif /* _3_FUNTION_LIKE_MACRO_H_ */
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef _4_SUM_MACRO_
+#define _4_SUM_MACRO_
+
+#define SUM(x, y) (x + y)
+
+#endif /* _4_SUM_MACRO_ */
