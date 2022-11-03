b6d9f27e6a683ce5c03687d4c68160b7258a9274
Montodel <elnenysuleiman@gmail.com>
2022-11-03 02:50:49 -0700

    UPDATE OF C - File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,7 +7,7 @@
  * Return: 1 if it success. -1 if it fails.
  */
 
-int create_file(const char *filename, char *text_content
+int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	int nletters;
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,39 +1,45 @@
 #include "main.h"
 
-/*
- * append_text_to_file - appends text at the end of a file
- * @filename: filename.
- * @text_content: added content.
- * Return: 1 if the file exists. -1 if the fails does not exist
- * or if it fails.
+/**
+ * append_text_to_file - appends text at the end of a file.
+ * @filename: name of the file and text_content
+ * @text_content: content text of file
+ * Return:1 on success, -1 on failure
  */
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
-	int nletters;
-	int rwr;
+	int da;
+	size_t l;
+	ssize_t wr;
 
 	if (!filename)
-		return (-1);
-
-	fd = open(filename, O_WRONLY | O_APPEND);
-
-	if (fd == -1)
-		return (-1);
-
-	if (text_content)
 	{
-		for (nletters = 0; text_content[nletters]; nletters++)
-			;
-
-		rwr = write(fd, text_content, nletters);
-
-		if (rwr == -1)
-			return (-1);
+		return (-1);
 	}
 
-	close(fd);
+	da = open(filename, O_RDWR | O_APPEND, 0600);
+	if (da == -1)
+	{
+		return (-1);
+	}
 
+	if (!text_content)
+	{
+		close(da);
+		return (1);
+	}
+
+	for (l = 0; text_content[l] != '\0'; l++)
+	{
+		;
+	}
+
+	wr = write(da, text_content, l);
+	if (wr == -1)
+	{
+		return (-1);
+	}
+	close(da);
 	return (1);
 }
